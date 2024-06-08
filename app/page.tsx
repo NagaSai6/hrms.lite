84970205426ad84cb2025cb8702a0fcfff80b18e
Naga Sai <naga@wishup.co>
2024-06-08 18:53:58 +0530

    #fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-200">
-      <div className="bg-white bg-opacity-25 backdrop-blur-lg shadow-lg p-8 rounded-xl">
-        <h1 className="text-5xl font-semibold text-gray-800 text-center mb-6">
+      <div className="w-11/12 sm:w-full max-w-2xl bg-white bg-opacity-25 backdrop-blur-lg shadow-lg p-4 sm:p-8 rounded-xl flex flex-col items-center">
+        <h1 className="text-3xl sm:text-5xl font-semibold text-gray-800 text-center mb-6">
           Welcome to HRMS Lite 🚀
         </h1>
-        <p className="text-lg text-gray-700 text-center mb-8">
+        <p className="text-sm sm:text-lg text-gray-700 text-center mb-8">
           Manage your small business HR needs efficiently and effectively
         </p>
 
